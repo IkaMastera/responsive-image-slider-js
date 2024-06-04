cab0b5eef431e22816b2104089425df2aa953aa6
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-06-04 12:09:34 +0400

    add: prev slide function

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ function next() {
   i = (j + i + 1) % j;
   document.getElementById("content" + (i + 1)).classList.add("active");
 }
+
+function prev() {
+  document.getElementById("content" + (i + 1)).classList.remove("active");
+  i = (j + i - 1) % j;
+  document.getElementById("content" + (i + 1)).classList.add("active");
+}
